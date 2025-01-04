50a42667958c4c84792a088f65a58df35e7f2ff0
Ram <ramki2537@gmail.com>
2025-01-04 09:19:08 +0800

    new commit on variables

--- variables1.sh
+++ variables1.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+person1=$1
+person2=$2
 
+echo "Today is which day: $person1"
+echo "We have outing on next week: $person2"
+echo "Going to india on next month: $person1"
+echo "Coming back to singapore on after two months: $person2"
+echo "best day of the lif is; $person1"
