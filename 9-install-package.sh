8a11aee418349c7a7a52bd7526c56b3428767854
Ram <ramki2537@gmail.com>
2025-01-04 12:55:06 +0800

    updated code

---
+++ 9-install-package.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then
+    echo "Error: you mush have admin previlges to run this script"
+    exit 1  # to exit code due to insufficient previges
+fi
+
+# check whether package installation
+
+dnf list installed mysql 
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then 
+    echo "MySQL package installation...FAILED"
+    exit 1
+    else
+    echo "MySQL package installation...SUCCESS"
+    fi
+else 
+    echo "MySWL package already.....INSTALLED"
+fi
+
+# git installation
+dnf lis installed git
+if [ $? -ne 0 ]
+then
+     dnf install git -y
+     if [ $? -ne 0 ]
+     then
+        echo "Git package installation....FAILED"
+        exit 1
+     else
+        echo "Git package installation....SUCCESS"
+     fi
+else
+    echo "Git package already....INSTALLED"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Git package installation...FAILED"
+    exit 1
+else
+    echo "Git package installation...SUCCESS"
+fi
