b802917a21cff3f63cd96141aa0b6ab016128c1f
Ram <ramki2537@gmail.com>
2025-01-04 11:36:26 +0800

    new addition script

---
+++ 7-special-var.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "All variables passed: $@"
+echo "Number of vairables: $#"
+echo "Script name: $0"
+echo "Pesent working directory; $PWD"
+echo "Home directory of present user: $Home"
+echo "Which user is running script: $USER"
+echo "Process id of current script: $$"
+echo "Previous command process id: $!"
