9a1cba9e9bd127d3249daacb4a5a40fa40159a36
Ram <ramki2537@gmail.com>
2025-01-04 11:17:22 +0800

    new addition script

--- 6.arrays.sh
+++ 6.arrays.sh
@@ -4,6 +4,6 @@ MOVIES=("pushpa" "rrr" "devara")
 
 echo "First movie: ${MOVIES[0]}"
 echo "Second movie: ${MOVIES[1]}"
-echo "Third movie: ${MOVIES[2]}
+echo "Third movie: ${MOVIES[2]}"
 
-echo "All movies: ${MOVIES[$@]}
+echo "All movies: ${MOVIES[@]}"
