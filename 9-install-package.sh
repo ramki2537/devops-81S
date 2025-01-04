849f87f1d96bd29ed2727d91181629f68d003040
Ram <ramki2537@gmail.com>
2025-01-04 13:14:47 +0800

    updated code

--- 9-install-package.sh
+++ 9-install-package.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-userid=$(id -u)
+USERID=$(id -u)
 
-if [ $userid -ne 0 ]
+if [ $USERID -ne 0 ]
 then
-    echo "Error: you mush have admin previlges to run this script"
+    echo "Error: you must have admin previlges to run this script"
     exit 1  # to exit code due to insufficient previges
 fi
 
@@ -15,30 +15,30 @@ then
     dnf install mysql -y
     if [ $? -ne 0 ]
     then 
-    echo "MySQL package installation...FAILURE"
-    exit 1
+          echo "MySQL package installation...FAILURE"
+          exit 1
     else
-    echo "MySQL package installation...SUCCESS"
+          echo "MySQL package installation...SUCCESS"
     fi
 else 
     echo "MySQL package already.....INSTALLED"
 fi
 
 # git installation
-dnf lis installed git
+dnf list installed git
 
 if [ $? -ne 0 ]
 then
      dnf install git -y
      if [ $? -ne 0 ]
      then
-        echo "Git package installation....FAILURE"
-        exit 1
+          echo "Git package installation....FAILURE"
+          exit 1
      else
-        echo "Git package installation....SUCCESS"
+          echo "Git package installation....SUCCESS"
      fi
 else
-    echo "Git package already....INSTALLED"
+     echo "Git package already....INSTALLED"
 fi
 
 
