1c69602587b5d6647af1b1bc82796fa758799b08
Ram <ramki2537@gmail.com>
2025-01-04 10:00:18 +0800

    new addition script

--- addition-of-twonumbers.sh
+++ addition-of-twonumbers.sh
@@ -5,7 +5,8 @@ value2=$2
 sum=$((value1+value2))
 timestamp=$(date)
 
-read -p echo "This scrip performed by: name"
+read -p echo "Enter your name: " name
 
+echo "This script performed by: $name"
 echo "This script performed on: $timestamp"
 echo "addition of $value1 and $value2 is : $sum
