4987ea95b39e2865bdc056235fc0e602bb0f1410
Ram <ramki2537@gmail.com>
2025-01-04 16:26:38 +0800

    updated code

--- 12.logs.sh
+++ 12.logs.sh
@@ -8,8 +8,8 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/rama"
-LOG_FILE=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE=$( echo $0 | cut -d "." -f1 )
+TIMESTAMP=$( date +%Y-%m-%d-%H-%M-%S )
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 
