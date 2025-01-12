de6c6089991d97d9688029b7db6a939a1072cf1e
Rama <ramki2537@gmail.com>
2025-01-12 23:19:47 +0800

    while prac

---
+++ 6-special-var.sh
@@ -0,0 +1,11 @@
+echo "All variables passed: $@"
+echo "Number of variables passed: $#"
+echo "Display passed values at a time: $*"
+echo "Script name: $0"
+echo "Present working directory: $PWD"
+echo "Home directory of the present user: $HOME"
+echo "Which user running script: $USER"
+echo "Process id of current user: $$"
+sleep 10 &
+echo "Process id of previous command: $!"
+echo "Previous command status: $?"
