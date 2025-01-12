9bc47497b85601a3cfd01f18c6266f8b3db3dce7
Rama <ramki2537@gmail.com>
2025-01-12 22:52:06 +0800

    while prac

--- 4-arrays.sh
+++ 4-arrays.sh
@@ -3,3 +3,7 @@
 movies=("hyderabad" "delhi" "Bangalore")
 
 echo "first city in india: ${movies[0]}"
+echo "second city in india: ${movies[1]}"
+echo "third city in india: ${movies[2]}"
+
+echo "All cities in india: ${movies[@]}"
