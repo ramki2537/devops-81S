92d571f3e707dd85bc246b499b09bbb495e705b6
Rama <ramki2537@gmail.com>
2025-01-12 23:01:10 +0800

    while prac

---
+++ 5-special-vars.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p "Enter 4 values: " value1 value2 value3 value4
+
+echo "All variables passed to the script: $@ "
+echo "Number of variables passed to the script: $#"
+echo "Number of variables passed in single string: $*"
+echo "Current script process id: $$"
+sleep 10 &
+echo "Previous command process id: $!"
+echo "Previous command execution status: $?"
+echo "Current script name: $0"
+echo "Script executing by: $USER"
+echo "Home directory of present user: $HOME"
+echo "Present working directory: $PWD"
+
