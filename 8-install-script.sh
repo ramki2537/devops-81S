a9d924969e8e723c108f30a1f1f37df440d49ef5
Rama <ramki2537@gmail.com>
2025-01-12 23:57:44 +0800

    while prac

--- 8-install-script.sh
+++ 8-install-script.sh
@@ -7,5 +7,22 @@ then
   echo "Admin check pass and proceeding installation"
 else
   echo "Error: You need admin access to perform this task"
+  exit 1
 fi
 
+dnf list installed mysql
+
+if [ $? -eq 0 ]
+then
+   echo "Mysql software already exisist in machine"
+   exit 1
+else 
+   dnf install mysql -y
+   if [ $? -eq 0 ]
+   then
+     echo "Installation for mysql success"
+   else
+     echo "Installation of mysql failed"
+     exit 1
+   fi
+fi
