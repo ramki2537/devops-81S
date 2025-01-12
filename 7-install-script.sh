bba87457f46d593169ea5d6f295abb7580dd9d62
Rama <ramki2537@gmail.com>
2025-01-12 23:46:12 +0800

    while prac

---
+++ 7-install-script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Error: You need admin previldues to peform this task"
+    exit 1
+fi
+
+dnf list installed 
+
+if [ $? -ne 0 ]
+then
+    dnf install nginx -y
+    if [ $? -ne 0 ]
+    then
+      echo "Installation of nginx....FAILED"
+      exit 1
+    else
+      echo "Installation of nginx....SUCCESS"
+    fi
+else
+   echo "Nginx already installed in this machine"
+fi
