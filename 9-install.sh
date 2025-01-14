9b8ede59d804171cb3b76f53a38cb5405b6d65c7
Rama <ramki2537@gmail.com>
2025-01-14 22:07:55 +0800

    while prac

---
+++ 9-install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then
+   echo -e "$G Error: You dont have sufficient access to perform this task $N"
+   exit 1
+fi
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+   echo -e "$2 installation...$R FAILED $N"
+else
+   echo -e "$2 installation...$G SUCCESS $N"
+}
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then 
+   dnf install nginx -y
+   VALIDATE $? "nginx"
+else
+   echo "nginx software already....$Y INSTALLED $N"
+fi
